2de7367754597f2b0adb65836dc4913e72d0aacd
Seongjun2 <tjdwns4302@naver.com>
2019-07-30 20:32:46 +0900

    uservo - 권한 주석

--- src/VO/UserVO.java
+++ src/VO/UserVO.java
@@ -1,13 +1,18 @@
 package VO;
 
 public class UserVO{
+
+
     private int uuid;
     private String tell;
     private String name;
     private String pw;
-    private int permission;
+    private int permission;// 1: 비로그인, 2: 로그인(회원), 3:로그인(프리미엄) ,10: 관리자
     private int point;
 
+    public int getUuid() { return uuid; }
+
+    public void setUuid(int uuid) { this.uuid = uuid; }
     public String getTell() {
         return tell;
     }
