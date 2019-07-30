01698abf63db100026e17768cc7960ccd9116cd8
LeagueLugas <blackblack9685@gmail.com>
2019-07-31 01:21:59 +0900

    상단 메뉴바 기능 추가!
    디자인은 안함

---
+++ web/html/js/common.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+	document.getElementById('main_header_menu_btn').onclick = function() {
+		document.getElementById('header_sidebar').style.transform = 'translate(0, -20%)';
+	}
+	document.getElementById('header_sidebar_blank').onclick = function() {
+		document.getElementById('header_sidebar').style.transform = 'translate(-60vw, -20%)';
+	}
+}
