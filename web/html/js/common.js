28af61a9d763ed1ec37d9171fe5dacf67fa17aef
LeagueLugas <blackblack9685@gmail.com>
2019-07-31 13:35:42 +0900

    메인 헤더 검색창 이펙트 추가
    야하!

--- web/html/js/common.js
+++ web/html/js/common.js
@@ -1,10 +1,20 @@
-window.onload = () => {
-	document.getElementById('main_header_menu_btn').onclick = function() {
-		document.getElementById('header_sidebar').style.transform = 'translate(0, -20%)';
-		document.getElementById('header_sidebar_blank').style['z-index'] = '1';
+window.addEventListener("click", function(e) {
+	switch(e.target.id) {
+		case 'header_sidebar':
+		case 'main_header_menu_btn':
+			document.getElementById('header_sidebar').style.transform = 'translate(0, -20%)';
+			break;
+		case 'main_header_search_text':
+			document.getElementById('main_header_search_span').style.border = '3px solid #000000';
+			document.getElementById('main_header_search_span').style.transform = 'translate(0, -3%)';
+			document.getElementById('main_header_search_span').style.height = '61%';
+			document.getElementById('main_header_search_span').style.width = '91%';
+			break;
+		default:
+			document.getElementById('header_sidebar').style.transform = 'translate(-60vw, -20%)';
+			document.getElementById('main_header_search_span').style.border = '0';
+			document.getElementById('main_header_search_span').style.transform = 'translate(0, 0)';
+			document.getElementById('main_header_search_span').style.height = '60%';
+			document.getElementById('main_header_search_span').style.width = '90%';
 	}
-	document.getElementById('header_sidebar_blank').onclick = function() {
-		document.getElementById('header_sidebar').style.transform = 'translate(-60vw, -20%)';
-		document.getElementById('header_sidebar_blank').style['z-index'] = '-1';
-	}
-}
+});
