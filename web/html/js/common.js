4bbe2acf1e795ffe389e935206c5c5a7e045b090
LeagueLugas <blackblack9685@gmail.com>
2019-07-31 11:39:21 +0900

    헤더 컴색부분 추가기능
    퍄하!

--- web/html/js/common.js
+++ web/html/js/common.js
@@ -1,8 +1,10 @@
 window.onload = () => {
 	document.getElementById('main_header_menu_btn').onclick = function() {
 		document.getElementById('header_sidebar').style.transform = 'translate(0, -20%)';
+		document.getElementById('header_sidebar_blank').style['z-index'] = '1';
 	}
 	document.getElementById('header_sidebar_blank').onclick = function() {
 		document.getElementById('header_sidebar').style.transform = 'translate(-60vw, -20%)';
+		document.getElementById('header_sidebar_blank').style['z-index'] = '-1';
 	}
 }
