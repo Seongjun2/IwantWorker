55d48e5305dab84c2639eb54e43ae621c4e7d7c3
LeagueLugas <blackblack9685@gmail.com>
2019-08-02 19:54:35 +0900

    [헤더] 사이드 바 수정

--- web/html/js/common.js
+++ web/html/js/common.js
@@ -1,10 +1,5 @@
 window.addEventListener("click", function(e) {
 	var id = e.target.id;
-	if(id === 'main_header_menu_btn_area' || id === 'main_header_menu_btn') {
-		document.getElementById('header_sidebar').style.transform = 'translate(0, -350px)';
-		document.getElementById('header_sidebar_blank').style.display = 'block';
-		document.body.style['overflow-y'] = 'hidden';
-	}
 	if(id === 'main_header_search_text') {
 		document.getElementById('main_header_search_span').style.border = '3px solid #000000';
 		document.getElementById('main_header_search_span').style.transform = 'translate(0, -3%)';
@@ -19,8 +14,13 @@ window.addEventListener("click", function(e) {
 });
 
 window.onload = function () {
+    document.getElementById('main_header_menu_btn_area').onclick = function() {
+        document.getElementById('header_sidebar').style.transform = 'translate(0, -350px) rotate(.001deg)';
+        document.getElementById('header_sidebar_blank').style.display = 'block';
+        document.body.style['overflow-y'] = 'hidden';
+    }
 	document.getElementById('header_sidebar_blank').onclick = function() {
-		document.getElementById('header_sidebar').style.transform = 'translate(-60vw, -350px)';
+		document.getElementById('header_sidebar').style.transform = 'translate(-60vw, -350px) rotate(.001deg)';
 		document.getElementById('header_sidebar_blank').style.display = 'none';
 		document.body.style['overflow-y'] = 'scroll';
 	}
