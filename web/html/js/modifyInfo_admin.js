0dff9ec298300e872f3586dbe2a0ea265fcd05a7
Seongjun2 <tjdwns4302@naver.com>
2019-08-04 22:41:23 +0900

    [회원정보수정] 연락처 숫자만 입력가능

--- web/html/js/modifyInfo_admin.js
+++ web/html/js/modifyInfo_admin.js
@@ -1,4 +1,3 @@
-
 function check_PW() {
     var pw = document.getElementById('pw').value;
     var pw_check = document.getElementById('pw_check').value;
@@ -7,4 +6,22 @@ function check_PW() {
         alert("비밀번호가 틀립니다.");
         event.preventDefault();
     }
+}
+
+function NumObj(obj){
+
+    if (event.keyCode >= 48 && event.keyCode <= 57) {
+
+    }else{
+
+        event.returnValue = false;
+
+    }
+
+}
+function SetNum(obj){
+    val=obj.value;
+    re=/[^0-9]/gi;
+    obj.value=val.replace(re,"");
+
 }
