f1fff19ce0aaae5c93007bac70396af73824220a
ssoso27 <sohi8290@gmail.com>
2019-08-05 11:58:33 +0900

    [Route] Board 추가

--- src/routes/Router.java
+++ src/routes/Router.java
@@ -6,11 +6,13 @@ public class Router {
     public final MainRoute main;
     public final MypageRoute mypage;
     public final AdminRoute admin;
+    public final BoardRoute board;
 
     private Router(String ctxPath) {
         main = new MainRoute( ctxPath );
         mypage = new MypageRoute( ctxPath );
         admin = new AdminRoute( ctxPath );
+        board = new BoardRoute( ctxPath );
     }
 
     public static Router getInstance(String ctxPath) {
