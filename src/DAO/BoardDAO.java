cf0be797ffbc51ae807ed445dc6cb85f95bb83d4
ssoso27 <sohi8290@gmail.com>
2019-08-06 02:48:38 +0900

    [mypage] BoardDAO 수정
    - findByUUID 추가
    - mapRow 추가
    - JdbcTemplate 설정

--- src/DAO/BoardDAO.java
+++ src/DAO/BoardDAO.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface BoardDAO{
   void add(BoardVO vo) throws Exception;
   List<BoardVO> findAll() throws Exception;
+  List<BoardVO> findByUUID(Integer Uuid) throws Exception;
 }
