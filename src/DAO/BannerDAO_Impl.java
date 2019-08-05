76b985940b5f998e399286eb679a05b21f8e800d
Seongjun2 <tjdwns4302@naver.com>
2019-08-05 15:26:39 +0900

    [배너업로드] 구현완료

--- src/DAO/BannerDAO_Impl.java
+++ src/DAO/BannerDAO_Impl.java
@@ -1,20 +1,50 @@
 package DAO;
 
 import VO.BannerVO;
+import database.JdbcTemplate;
 import database.RowMapper;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BannerDAO_Impl implements BannerDAO, RowMapper<BannerVO> {
+    private JdbcTemplate template = null;
+
     @Override
     public void add(BannerVO vo) throws Exception {
+        template = new JdbcTemplate();
 
+        String sql = "insert into banner (Image) values ( ? )";
+        int result = template.update(sql, vo.getImage());
+
+        if(result < 1){
+            System.out.println("변경된 것이 없습니다.");
+        }
     }
 
     @Override
     public List<BannerVO> findAll() throws Exception {
-        return null;
+        List<BannerVO> list = new ArrayList<>();
+        template = new JdbcTemplate();
+        RowMapper<BannerVO> rowMapper = new BannerDAO_Impl();
+
+        String sql = "select * from banner";
+        list = template.query(sql,rowMapper);
+
+        for(BannerVO vo :list){
+            System.out.println(vo.getImage());
+        }
+
+        return list;
+    }
+
+    @Override
+    public void removeAll() throws Exception {
+        template = new JdbcTemplate();
+        String sql = "delete from banner where 1=1";
+        int result = template.update(sql);
+        System.out.println("삭제한 row 수 : " + result);
     }
 
     @Override
@@ -22,7 +52,6 @@ public class BannerDAO_Impl implements BannerDAO, RowMapper<BannerVO> {
         BannerVO vo = new BannerVO();
         vo.setBanner_id(rs.getInt("Banner_Id"));
         vo.setImage(rs.getString("Image"));
-        vo.setUploadTime(rs.getString("UploadTime"));
         return vo;
     }
 }
--- web/html/adm/upload.jsp
+++ web/html/adm/upload.jsp
@@ -7,23 +7,23 @@
 <%@ page import="DAO.BannerDAO_Impl" %>
 <%@ page import="VO.BannerVO" %>
 <%
-
     String uploadPath = request.getServletContext().getRealPath("/WEB-INF/uploadImg");
-    FileManager fileManager = new FileManager();
-    fileManager.removeAll(uploadPath);
-
     System.out.println(("절대경로 : " + uploadPath));
 
-    MultipartRequest mpr = new MultipartRequest(
+    BannerDAO bannerDAO = new BannerDAO_Impl();
+    BannerVO bannerVO = new BannerVO();
+    FileManager fileManager = new FileManager();
+
+    fileManager.removeAll(uploadPath);//서버에 이미지 파일 모두 삭제
+    bannerDAO.removeAll();//table의 data 모두 삭제
+
+    MultipartRequest mpr = new MultipartRequest(//이미지 업로드
             request, uploadPath, 1024*1024*20, "UTF-8", new DefaultFileRenamePolicy()
     );
 
     Enumeration files = mpr.getFileNames();
 
-    BannerDAO bannerDAO = new BannerDAO_Impl();
-    BannerVO bannerVO = new BannerVO();
-
-    for (Enumeration  e = mpr.getFileNames(); e.hasMoreElements() ;) {
+    for (Enumeration  e = mpr.getFileNames(); e.hasMoreElements() ;) {//데이터 베이스 삽입.
         String strName = (String) e.nextElement();
         String fileName= mpr.getFilesystemName(strName);
         if (fileName != null) {
@@ -31,13 +31,10 @@
             System.out.println("fileName : " + fileName);
             bannerVO.setImage(fileName);
         }
+        bannerDAO.add(bannerVO);
     }
-
-//    bannerDAO.add();
-
-    //MultipartRequest를 쓰면 request.getParameter 못씀
-    // 대신 MultipartRequest 안에 있는 getParameter 써야 함.
-    // 한글 처리도 내부에서 해줌.
+    bannerDAO.findAll();//테스트
+    response.sendRedirect("./userList_Admin.jsp");
 
 %>
 <html>
