5610620595dde3d7dbbe44d5f117bd94fb51a400
ssoso27 <sohi8290@gmail.com>
2019-08-06 19:20:16 +0900

    [포인트기록] PointLogDAO 수정
    - JdbcTemplate 추가
    - findByUUID 메소드 추가

--- src/DAO/PointLogDAO_Impl.java
+++ src/DAO/PointLogDAO_Impl.java
@@ -1,10 +1,17 @@
 package DAO;
 
 import VO.PointLogVO;
+import database.JdbcTemplate;
 
 import java.util.List;
 
 public class PointLogDAO_Impl implements PointLogDAO {
+    private JdbcTemplate template = null;
+
+    public PointLogDAO_Impl() {
+        this.template = new JdbcTemplate();
+    }
+
     @Override
     public void add(PointLogVO vo) throws Exception {
 
@@ -13,5 +20,11 @@ public class PointLogDAO_Impl implements PointLogDAO {
     @Override
     public List<PointLogVO> findAll() throws Exception {
         return null;
+    }
+
+    @Override
+    public List<PointLogVO> findByUUID(Integer uuid) throws Exception {
+        List<PointLogVO> pointLogs = null;
+        return pointLogs;
     }
 }
