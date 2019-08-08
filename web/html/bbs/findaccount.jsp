d11e83a1dee7332f1c0c99fdd82f3e88b0cb0297
woochanlee <030219woo@naver.com>
2019-08-09 02:00:09 +0900

    [로그인,회원가입,계정찾기] 다 되써~?~~?
    예외 알려주세요@!!!@!@!

--- web/html/bbs/findaccount.jsp
+++ web/html/bbs/findaccount.jsp
@@ -14,7 +14,7 @@
 %>
 <%@include file="../header.jsp"%>
 <script type="text/javascript" src="../js/modifyInfo_admin.js"></script>
-<script type="text/javascript" src="../js/findaccount.js.js"></script>
+<script type="text/javascript" src="../js/findaccount.js"></script>
 <link rel="stylesheet" type="text/css" , href="<%=cssDir%>/findaccount.css">
 <main>
     <div class="div_pageName">
@@ -23,8 +23,8 @@
     <div class="div_mainDiv">
         <h1>본인의 전화번호를 입력해주세요</h1>
         <form method="post" action="findaccount2.jsp">
-            <input id="text" max="9" min="0" pattern="\d*" onkeydown="return max_length();" type="number" name="tell" placeholder="(-) 하이폰 없이 숫자만 입력" />
-            <input type="submit" id="input_submit">
+            <input id="text" pattern="\d*" onKeyup="this.value=this.value.replace(/[^0-9]/g,'');" onkeydown="return max_length( event );" onkeypress="submit_enter(event)" type="number" name="tell" placeholder="(-) 하이폰 없이 숫자만 입력" />
+            <input type="button" id="input_submit" value="제출" onclick="submit()">
         </form>
 
     </div>
--- web/html/js/findaccount.js
+++ web/html/js/findaccount.js
@@ -1,7 +1,42 @@
-function max_length() {
-    if (document.getElementById("tell").value.length >=  11 ) {
+function max_length( event ) {
+    if ( event.keyCode === 9 || event.keyCode === 13 || event.keyCode === 116 || event.keyCode === 17 || event.keyCode === 8 || event.keyCode === 16 ) {
+        return true;
+    }
+    if (document.getElementById("text").value.length >=  11 ) {
         return false;
     } else {
         return true;
     }
 }
+function  submit() {
+    var tell = document.getElementById("tell");
+    var pw = document.getElementById("pw");
+    var name = document.getElementById("name");
+    var answer = document.getElementById("answer");
+
+    if ( tell.value.indexOf(" ") != -1 || pw.value.indexOf(" ") != -1 || name.value.indexOf(" ") != -1 || answer.value.indexOf(" ") != -1) {
+        alert("공백은 입력할수 없습니다.");
+        event.preventDefault();
+        return;
+    } else if (isNaN(tell.value)) {
+        alert("전화번호에는 숫자만 입력이 가능합니다.");
+        event.preventDefault();
+        return;
+    } else if (tell.value == "" || name.value == "" || pw.value == "" || answer.value == "") {
+        alert("빈칸은 입력할 수 없습니다.");
+        event.preventDefault();
+        return;
+    } else if (tell.value.length != 11) {
+        alert("전화번호 11자리를 입력해주세요");
+        event.preventDefault();
+        return;
+    } else {
+        document.getElementById("login_form").submit();
+    }
+
+}
+function submit_enter (event) {
+    if ( event.code === 'Enter' || event.code === 'NumpadEnter' ) {
+        submit();
+    }
+};
